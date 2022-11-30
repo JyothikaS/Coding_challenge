ca9cf1d0d39c5fab4d175c7818f51648ba980a69
Jyothika Suresh <62376533+JyothikaS@users.noreply.github.com>
2022-11-30 09:55:27 +0530

    Add files via upload

---
+++ Q5_evodd.js
@@ -0,0 +1,14 @@
+var flight_fare = [];
+var input = require('readline-sync');
+
+for (let i = 0; i < (4 * 2); i++) {
+    if (i % 2 == 0) {
+        var flight = parseFloat(input.question("enter the flight fare: "));
+        flight_fare[i] = flight
+    }
+    else {
+        flight_fare[i] = ' '
+    }
+}
+console.log("Flight fare: ")
+console.log(flight_fare);
